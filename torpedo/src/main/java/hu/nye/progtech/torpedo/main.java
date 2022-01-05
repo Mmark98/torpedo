a670f121ed626c23cf36cd14dedb49021a38cb32
Mmark98 <58510020+Mmark98@users.noreply.github.com>
2022-01-05 16:42:34 +0100

    Add files via upload

--- torpedo/src/main/java/hu/nye/progtech/torpedo/main.java
+++ torpedo/src/main/java/hu/nye/progtech/torpedo/main.java
@@ -69,11 +69,15 @@ class BattleShips {
             int row = input.nextInt();
 
 
-            if (row > 9 || col > 9) {//nem helyezhetsz hajokat a 10 x 10 racson kivulre
+            if (row > 9 || col > 9)
+            {//nem helyezhetsz hajokat a 10 x 10 racson kivulre
                 System.out.println("A megadott koordinata tartomanyon kivül esik. Kerjuk, probalja ujra");
-            } else if (ocean[row][col] != null) { //NEM helyezhet el ket vagy tobb hajot ugyanarra a helyre
+            } else if (ocean[row][col] != null)
+            { //NEM helyezhet el ket vagy tobb hajot ugyanarra a helyre
                 System.out.println("A megadott koordinata mar hasznalatban van. Kerjuk, probalja ujra");
-            } else {
+            }
+            else
+            {
                 ocean[row][col] = "@";
                 userShips++;
             }
@@ -83,10 +87,13 @@ class BattleShips {
     public static void compLoc(String[][] ocean) {
         System.out.println("\n\nA szamitogep hajokat telept:");
 
-        while (computerShips < 5) {
-            int row = rand.nextInt(10), col = rand.nextInt(10); //nem helyezhetsz hajokat a 10 x 10 racson kivulre
+        while (computerShips < 5)
+        {
+            int row = rand.nextInt(10);
+            int col = rand.nextInt(10); //nem helyezhetsz hajokat a 10 x 10 racson kivulre
 
-            if (ocean[row][col] == null && ocean2[row][col] == null) {//nem helyezheti el a hajot  olyan helyre, amelyet mar elfoglalt egy masik hajo (jatekos vagy szamitogep)
+            if (ocean[row][col] == null && ocean2[row][col] == null)
+            {//nem helyezheti el a hajot  olyan helyre, amelyet mar elfoglalt egy masik hajo (jatekos vagy szamitogep)
                 System.out.println("Hajo telepitve. ");
                 ocean2[row][col] = "@";//hajok helye az ocean2 on
                 computerShips++;
@@ -100,28 +107,34 @@ class BattleShips {
         System.out.println("\n\nszamitogep kore");
         int turn = 0;
 
-        while (turn == 0) {
+        while (turn == 0)
+        {
 
             int x = rand.nextInt(10);
-            ;
             int y = rand.nextInt(10);
-            ;
 
-            if (ocean2[x][y] == "@") {//A szamitogep kitalalta a sajat hajojanak koordinatait (a szamitogep elvesziti a hajot).
+
+            if (ocean2[x][y] == "@")
+            {//A szamitogep kitalalta a sajat hajojanak koordinatait (a szamitogep elvesziti a hajot).
                 System.out.println("a szamitogep elsullyesztette a saját hajojat !");
                 ocean[x][y] = "!";
                 ocean2[x][y] = "!";
                 computerShips--;
                 turn++;
-            } else if (ocean[x][y] == "@") { //A szamitogep kitalalta a jatekos hajojanak koordinatait (a jatekos elvesziti a hajot).
+            } else if (ocean[x][y] == "@")
+            { //A szamitogep kitalalta a jatekos hajojanak koordinatait (a jatekos elvesziti a hajot).
                 System.out.println("a szamítogep elsullyesztette a hajodat!");
                 ocean[x][y] = "+";
                 ocean2[x][y] = "+";
                 userShips--;
                 turn++;
-            } else if (ocean[x][y] == "!" || ocean[x][y] == "x" || ocean2[x][y] == "-") {
+            } else if (ocean[x][y] == "!" || ocean[x][y] == "+" || ocean2[x][y] == "X")
+            {
 
-            } else {//Szamitogep tevesztett. Nincs hajo a kitalalt koordinatakon.
+            }
+            else
+            {
+                //Szamitogep tevesztett. Nincs hajo a kitalalt koordinatakon.
                 System.out.println("szamitogep nem talalt el a hajodat!");
                 ocean2[x][y] = "X";
                 turn++;
@@ -134,7 +147,8 @@ class BattleShips {
         System.out.println("\nA te korod");
         int turn = 0;
 
-        while (turn == 0) {
+        while (turn == 0)
+        {
 
             System.out.print("Adja meg az X koordinatat: ");
             int col = input.nextInt();
@@ -142,22 +156,35 @@ class BattleShips {
             int row = input.nextInt();
 
 
-            if (row > 9 || col > 9) {//nem lohetsz a 10x10-es racson kivul
+            if (row > 9 || col > 9)
+            {
+                //nem lohetsz a 10x10-es racson kivul
                 System.out.println("A megadott koordinata tartomanyon kivül esik. Kerjuk, probalja ujra.\n");
-            } else if (ocean[row][col] == "!" || ocean[row][col] == "x" || ocean[row][col] == "-") {
+            }
+            else if (ocean[row][col] == "!" || ocean[row][col] == "x" || ocean[row][col] == "-")
+            {
                 System.out.println("A megadott koordinata mar hasznalatban volt. Kerjuk, probalja ujra.\n");
-            } else if (ocean2[row][col] == "@") {//A jatekos helyesen talalta ki a szamitogep hajojanak koordinatait (a szamitogep elvesziti a hajojat).
+            }
+            else if (ocean2[row][col] == "@")
+            {
+                //A jatekos helyesen talalta ki a szamitogep hajojanak koordinatait (a szamitogep elvesziti a hajojat).
                 System.out.println("Gratula! Elsullyesztetted a szamitogep hajojat!");
                 ocean[row][col] = "+";
                 ocean2[row][col] = "+";
                 computerShips--;
                 turn++;
-            } else if (ocean[row][col] == "@") { //A jatekos megadta a sajat hajoja koordinatait (a jatekos elvesziti a hajot).
+            }
+            else if (ocean[row][col] == "@")
+            {
+                //A jatekos megadta a sajat hajoja koordinatait (a jatekos elvesziti a hajot).
                 System.out.println("O nem, elsullyesztetted a sajat hajodat! ");
                 ocean[row][col] = "-";
                 userShips--;
                 turn++;
-            } else {//Jatekos tevesztett. nincs hajo ezzen a koordinatan
+            }
+            else
+            {
+                //Jatekos tevesztett. nincs hajo ezzen a koordinatan
                 System.out.println("Bocsi, teves!");
                 ocean[row][col] = "X";
                 turn++;
@@ -165,10 +192,11 @@ class BattleShips {
         }
     }
     public static void battle() {
-        printMap(ocean); //feltoltott terkep kiirasa
+        printMap(ocean);//feltoltott terkep kiirasa
         System.out.println("\nA te hajod: " + userShips + " | szamitogep hajo: " + computerShips + "\n-----------------------------------");
 
-        while (userShips > 0 && computerShips > 0) {
+        while (userShips > 0 && computerShips > 0)
+        {
             userTurn();
             computerTurn();
             printMap(ocean);
@@ -176,10 +204,10 @@ class BattleShips {
         }
 
         if (userShips == 0) {
-            System.out.println("\n*** Jatek vege ***");
+            System.out.println("\n Jatek vege ");
             System.out.println("\nA te hajod: " + userShips + " | szamitogep hajo: " + computerShips + "\n-------------------------------");
         } else if (computerShips == 0) {
-            System.out.println("gartulalok! Megnyerted a csatat :)");
+            System.out.println("Gratulalok! Megnyerted a csatat :)");
             System.out.println("\nA te hajod: " + userShips + " | szamitogep hajo: " + computerShips + "\n-------------------------------");
         }
         printMap2(ocean2);// teszt
